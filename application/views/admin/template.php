1c1d44da1934ad98b07912e44e132894db918435
rpariona81 <ronald.pariona.isidro@gmail.com>
2023-05-20 01:12:08 -0500

    ajustes de crud convocatorias y status y datatable

--- application/views/admin/template.php
+++ application/views/admin/template.php
@@ -159,6 +159,7 @@
     <script src="https://cdn.jsdelivr.net/npm/simple-datatables@latest" crossorigin="anonymous"></script>
 
     <script src="https://cdn.datatables.net/1.13.4/js/jquery.dataTables.min.js"></script>
+    <!--<script src="https://cdn.datatables.net/1.13.4/js/dataTables.bootstrap5.min.js"></script>-->
     <script src="https://cdn.datatables.net/buttons/2.3.6/js/dataTables.buttons.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jszip/3.1.3/jszip.min.js"></script>
     <script src="https://cdn.datatables.net/buttons/2.3.6/js/buttons.html5.min.js"></script>
