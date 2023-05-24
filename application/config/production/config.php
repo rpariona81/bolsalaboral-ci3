b91b980a350518f5342dd57fd5d006e96950b6d6
rpariona81 <ronald.pariona.isidro@gmail.com>
2023-05-23 23:33:10 -0500

    config/production cn getenv

--- application/config/production/config.php
+++ application/config/production/config.php
@@ -23,8 +23,8 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://134.65.252.9/';
-
+//$config['base_url'] = 'http://134.65.252.9/';
+$config['base_url'] = getenv('app.baseURL');
 /*
 |--------------------------------------------------------------------------
 | Index File
